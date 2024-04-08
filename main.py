7962699c395a9c891537ae8190e147fcaa7201c8
lameesmohee <lameesmohee2002@gmail.com>
2024-04-08 13:05:32 +0200

    Added Finding Keypoints in SIFT

--- main.py
+++ main.py
@@ -17,10 +17,9 @@ class MainWindow(QTabWidget):
         loadUi(ui_file, self)
         self.full_screen = False
         self.pushButton_browseImage.clicked.connect(self.browse_image)
-        
         self.template_matching = Template_Matching(self)
         self.addSIFT = SIFTCornerDetection(self)
-        self.template_matching.handle_buttons()   
+        self.template_matching.handle_buttons()
 
 
     def browse_image(self):
