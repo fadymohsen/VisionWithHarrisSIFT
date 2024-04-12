e4a9994e756a8ba3ad7050afffb6c986db8ca714
Fady Mohsen <82896079+fadymohsen@users.noreply.github.com>
2024-04-12 10:20:48 +0200

    Update Harris & Adding Keywords

--- HarrisCorner.py
+++ HarrisCorner.py
@@ -6,6 +6,9 @@ from PyQt5.QtWidgets import QLabel
 import time
 
 
+
+
+
 # img = cv2.imread('images\\free-printable-chess-board1.jpg')
 # cv2.imshow('img',img)
 # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -19,23 +22,32 @@ import time
 # print("Computation Time in Harris corner using OpenCV:", execution_time, "seconds") # approximately equal 0.0034122467041015625 seconds
 # cv2.imshow('dst', img)
 # if cv2.waitKey(0) == 27:
-#     cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
+
+
+
 
 
 class HarrisCornerDetection():
     def __init__(self, main_tab_widget):
         self.main_tab_widget = main_tab_widget
         self.ui = self.main_tab_widget
-        self.threshold_ratio = 0.01  # Initialize threshold ratio 1/100
+        # Initialize threshold ratio 1/100
+        self.threshold_ratio = 0.01  
         self.ui.pushButton.clicked.connect(self.detect_corners)
         self.ui.horizontalSlider_6.valueChanged.connect(self.update_threshold)
-        self.ui.horizontalSlider_6.setMinimum(0)  # Set the minimum value for the slider
-        self.ui.horizontalSlider_6.setMaximum(500)  # Set the maximum value for the slider
-        self.ui.horizontalSlider_6.setSingleStep(1)  # Set the step for the slider
+        # Set the minimum value for the slider
+        self.ui.horizontalSlider_6.setMinimum(0)
+        #  Set the maximum value for the slider
+        self.ui.horizontalSlider_6.setMaximum(500)
+        # Set the step for the slider
+        self.ui.horizontalSlider_6.setSingleStep(1)  
          # Create a QLabel to display the current threshold value
         self.threshold_label = QLabel()
-        self.ui.horizontalLayout_23.addWidget(self.threshold_label)  # Add the label to the layout
-        self.update_threshold_label(self.ui.horizontalSlider_6.value())  # Update the label initially
+        # Add the label to the layout
+        self.ui.horizontalLayout_23.addWidget(self.threshold_label)
+        # Update the label initially
+        self.update_threshold_label(self.ui.horizontalSlider_6.value())
 
 
     def upload_image(self):
@@ -52,13 +64,12 @@ class HarrisCornerDetection():
 
 
     def update_threshold(self, value):
-
         # Update the threshold ratio when the spinbox value changes
         self.threshold_ratio = value / 1000.0  
-
         # Trigger corner detection whenever the threshold changes
         self.detect_corners()
         self.update_threshold_label(value)
+
 
     def update_threshold_label(self, value):
         # Update the text of the threshold label
@@ -67,57 +78,43 @@ class HarrisCornerDetection():
 
 
     def detect_corners(self):
-
         # Start the timer
         start_time = time.time()
-
         # Convert image to float32
         image = np.float32(self.original_image)
-
         # Calculate derivatives (image gradients) using Sobel operator
         sobel_x = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=3)
         sobel_y = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=3)
-
         # Calculate products of derivatives
         IxIx = pow(sobel_x, 2)
         IxIy = sobel_x * sobel_y
         IyIy = pow(sobel_y, 2)
-
         # Apply Gaussian filter (3*3 kernel size) to the products of derivatives
         sum_IxIx = cv2.GaussianBlur(IxIx, (3,3), 0)  
         sum_IyIy = cv2.GaussianBlur(IyIy, (3,3), 0)    
         sum_IxIy = cv2.GaussianBlur(IxIy, (3,3), 0) 
-
         # Harris Corner Response Calculation
         k = 0.04   # commonly used value for k
         det_H = (sum_IxIx * sum_IyIy) - pow(sum_IxIy, 2)  # eigenValues multiplication
         trace_H = sum_IxIx + sum_IyIy                     # eigenValues summation
         harris_response = det_H - k * pow(trace_H, 2)
-
         # Thresholding
         threshold = self.threshold_ratio * np.max(harris_response)
         # print(np.max(harris_response))
-
         # Non-maximum suppression
         neighborhood_size = 1
         harris_response_max = cv2.dilate(harris_response, np.ones((neighborhood_size, neighborhood_size)))
         corner_mask = (harris_response == harris_response_max) & (harris_response > threshold)
-
-
         # Highlight corners wit red spots
         corners_image = cv2.cvtColor(self.original_image, cv2.COLOR_GRAY2BGR)
         corners_image[corner_mask] = [255, 0, 0]  
-
-
         # Display the image with the detected corners
         self.ui.graphicsLayoutWidget_afterHarris.clear()
         corners_img_item = pg.ImageItem(corners_image)
         corners_view = self.ui.graphicsLayoutWidget_afterHarris.addViewBox()
         corners_view.addItem(corners_img_item)
-
         # Stop the timer
         end_time = time.time()
-
         # Calculate the computation time only if it's the first time
         if not hasattr(self, 'computation_time'):
             self.computation_time = end_time - start_time
@@ -126,5 +123,3 @@ class HarrisCornerDetection():
             self.ui.textEdit.clear()
             # Display computation time on QTextEdit
             self.ui.textEdit.append("{:.4f} seconds".format(self.computation_time))
-
-        # print(self.threshold_ratio)
